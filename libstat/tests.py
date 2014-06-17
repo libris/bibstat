122e176398ce8428c79b38457afb3ecdc3255da9
Ann-Cathrin Näsström <ann-cathrin.nasstrom@valtech.se>
2014-06-17 15:19:54 +0200

    Added tests for management command import_variables

--- libstat/tests.py
+++ libstat/tests.py
@@ -3,6 +3,7 @@ from django.test.simple import DjangoTestSuiteRunner
 from django.test import TestCase
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.core.management import call_command
 
 from datetime import datetime
 import json
@@ -36,6 +37,130 @@ class MongoTestCase(TestCase):
         connection = get_connection()
         connection.drop_database(self.mongodb_name)
         disconnect()
+
+"""
+    Management command test cases
+"""
+class ImportVariablesTest(MongoTestCase):
+
+    def test_import_variables_requires_file_option(self):
+        args = []
+        opts = {"target_group": "public"}
+        call_command('import_variables', *args, **opts)
+        
+        self.assertEquals(len(Variable.objects.all()), 0)
+
+
+    def test_import_variables_requires_target_group_option(self):
+        args = []
+        opts = {"target_group": "public"}
+        call_command('import_variables', *args, **opts)
+        
+        self.assertEquals(len(Variable.objects.all()), 0)
+    
+    
+    def test_should_import_public_lib_variables(self):
+        args = []
+        opts = {"file": "data/folk_termer.xlsx", "target_group": "public"}
+        call_command('import_variables', *args, **opts)
+        
+        # Check that all variables have been imported
+        self.assertEquals(len(Variable.objects.all()), 201)
+        
+        folk1 = Variable.objects.filter(key="Folk1")[0] # Private (by category "Bakgrundsvariabel"), type "Text"
+        folk7 = Variable.objects.filter(key="Folk7")[0] # Private, type "Numerisk"
+        folk8 = Variable.objects.filter(key="Folk8")[0] # Public, type "Boolesk"
+        folk26 = Variable.objects.filter(key="Folk26")[0] # Public, type "Decimal två"
+        folk52 = Variable.objects.filter(key="Folk52")[0] # Public, type "Procent"
+        folk54 = Variable.objects.filter(key="Folk54")[0] # Public, type "Decimal ett"
+        folk201 = Variable.objects.filter(key="Folk201")[0] # Private, type "Integer", last row
+        
+        #  Check visibility
+        self.assertFalse(folk1.is_public)
+        self.assertFalse(folk7.is_public)
+        self.assertTrue(folk8.is_public)
+        self.assertTrue(folk26.is_public)
+        self.assertTrue(folk52.is_public)
+        self.assertTrue(folk54.is_public)
+        self.assertFalse(folk201.is_public)
+        
+        # Check types
+        self.assertEquals(folk1.type, u"string")
+        self.assertEquals(folk7.type, u"string")
+        self.assertEquals(folk8.type, u"boolean")
+        self.assertEquals(folk26.type, u"decimal")
+        self.assertEquals(folk52.type, u"percent")
+        self.assertEquals(folk54.type, u"decimal")
+        self.assertEquals(folk201.type, u"integer")
+        
+    
+    def test_should_update_public_lib_variables(self):
+        args = []
+        opts = {"file": "data/folk_termer.xlsx", "target_group": "public"}
+        call_command('import_variables', *args, **opts)
+        
+        # Check that all variables have been imported
+        self.assertEquals(len(Variable.objects.all()), 201)
+        # Check target_group before
+        self.assertEquals(Variable.objects.filter(key="Folk52")[0].target_groups, [u"public"])
+
+        # Changing target group to avoid having to modify terms file
+        args = []
+        opts = {"file": "data/folk_termer.xlsx", "target_group": "school"}
+        call_command('import_variables', *args, **opts)
+        
+        # Check that no new variables have been created
+        self.assertEquals(len(Variable.objects.all()), 201)
+         # Check target_group after
+        self.assertEquals(Variable.objects.filter(key="Folk52")[0].target_groups, [u"school"])
+        
+        
+    def test_should_import_research_lib_variables(self):
+        args = []
+        opts = {"file": "data/forsk_termer.xlsx", "target_group": "research"}
+        call_command('import_variables', *args, **opts)
+        
+        # Check that all variables have been imported
+        self.assertEquals(len(Variable.objects.all()), 163)
+        
+        forsk1 = Variable.objects.filter(key="Forsk1")[0] # Private (by category "Bakgrundsvariabler", type "Text"
+        forsk2 = Variable.objects.filter(key="Forsk2")[0] # Private, type "Integer"
+        forsk8 = Variable.objects.filter(key="Forsk8")[0] # Public, type "Decimal två"
+        forsk154 = Variable.objects.filter(key="Forsk154")[0] # Public, type "Decimal ett"
+        
+        # Check visibility
+        self.assertEquals(forsk1.is_public, False)
+        self.assertEquals(forsk2.is_public, False)
+        self.assertEquals(forsk8.is_public, True)
+        self.assertEquals(forsk154.is_public, True)
+        
+        # Check types
+        self.assertEquals(forsk1.type, u"string")
+        self.assertEquals(forsk2.type, u"integer")
+        self.assertEquals(forsk8.type, u"decimal")
+        self.assertEquals(forsk154.type, u"decimal")
+    
+    
+    def test_should_update_research_lib_variables(self):
+        args = []
+        opts = {"file": "data/forsk_termer.xlsx", "target_group": "research"}
+        call_command('import_variables', *args, **opts)
+        
+        # Check that all variables have been imported
+        self.assertEquals(len(Variable.objects.all()), 163)
+        # Check target_group before
+        self.assertEquals(Variable.objects.filter(key="Forsk111")[0].target_groups, [u"research"])
+        
+        # Changing target group to avoid having to modify terms file
+        args = []
+        opts = {"file": "data/forsk_termer.xlsx", "target_group": "hospital"}
+        call_command('import_variables', *args, **opts)
+        
+        # Check that no new variables have been created
+        self.assertEquals(len(Variable.objects.all()), 163)
+        # Check target_group after
+        self.assertEquals(Variable.objects.filter(key="Forsk111")[0].target_groups, [u"hospital"]) 
+
         
 """
     Util functions test cases
