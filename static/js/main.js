e18fb2062e5dab4cb8cd218c9e60bb3a1f6017e8
Viktor Lövgren <viktor.lovgren@valtech.se>
2014-10-02 15:24:42 +0200

    Move remaining code in js/libstat.js to js/app/main.js

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,7 @@
     'use strict';
 
     requirejs.config({
+<<<<<<< HEAD
         baseUrl: '/static/js/',
         shim: {
             'bootstrap': ['jquery'],
@@ -24,6 +25,30 @@
         },
 
         deps: ['app/main']
+=======
+        shim: {
+            'jquery.tablesorter': ['jquery'],
+            'bootstrap': ['jquery'],
+            'bootstrap.datepicker': ['bootstrap'],
+            'bootstrap.tokenfield': ['bootstrap'],
+            'typeahead': ['jquery']
+        },
+
+        paths: {
+            'jquery': ['/static/js/plugins/jquery/1.11.1/jquery'],
+            'jquery.tablesorter': ['/static/js/plugins/jquery/tablesorter/2.17.8/jquery.tablesorter.min'],
+            'bootstrap': ['/static/js/plugins/bootstrap/3.1.1/js/bootstrap'],
+            'bootstrap.datepicker': ['/static/js/plugins/bootstrap-datepicker/1.3.0/bootstrap-datepicker'],
+            'bootstrap.tokenfield': ['/static/js/plugins/bootstrap-tokenfield/0.12.0/bootstrap-tokenfield'],
+            'typeahead': ['/static/js/plugins/typeaheadjs/0.10.5/typeahead.bundle'],
+            'underscore': ['/static/js/plugins/underscorejs/1.7.0/underscore-min'],
+
+            // App
+            'sum': '/static/js/app/sum'
+        },
+
+        deps: ['/static/js/app/main.js']
+>>>>>>> Move remaining code in js/libstat.js to js/app/main.js
     });
 
     requirejs.onError = function(e) {
