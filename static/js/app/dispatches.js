95ec3a3293ae47d44862edaa46a83d77315f3cc2
Viktor Lövgren <viktor.lovgren@valtech.se>
2014-10-28 11:56:22 +0100

    Add missing dispatches.js file.

---
+++ static/js/app/dispatches.js
@@ -0,0 +1,12 @@
+define(['jquery'], function ($) {
+    var form = function () { return $('.form-dispatches'); };
+    var submitTo = function(url) { form().get(0).setAttribute('action', Urls[url]()); };
+    var init = function () {
+        form().find('.btn-delete').click(function () { submitTo('dispatches_delete'); });
+        form().find('.btn-send').click(function () { submitTo('dispatches_send'); });
+    };
+
+    return {
+        'init': init
+    }
+});
