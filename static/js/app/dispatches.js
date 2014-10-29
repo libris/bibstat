4f1ba18ebeb54ac1fc8d788f3e5e86f79a21c99f
Viktor Lövgren <viktor.lovgren@valtech.se>
2014-10-29 16:32:03 +0100

    More consistent look for dispatches.html

--- static/js/app/dispatches.js
+++ static/js/app/dispatches.js
@@ -1,6 +1,6 @@
 define(['jquery'], function ($) {
     var form = function () { return $('.form-dispatches'); };
-    var submitTo = function(url) { form().get(0).setAttribute('action', Urls[url]()); };
+    var submitTo = function(url) { form().get(0).setAttribute('action', Urls[url]()); form().submit(); };
     var init = function () {
         form().find('.btn-delete').click(function () { submitTo('dispatches_delete'); });
         form().find('.btn-send').click(function () { submitTo('dispatches_send'); });
