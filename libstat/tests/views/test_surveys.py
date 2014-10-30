7733af2b35a30f32fd98a5812d8080fe4b8ebd3e
Niclas Nilsson <niclas.love.nilsson@gmail.com>
2014-10-30 12:25:39 +0100

    Extend tests to check html

--- libstat/tests/views/test_surveys.py
+++ libstat/tests/views/test_surveys.py
@@ -59,22 +59,23 @@ class TestSurveyAuthorization(MongoTestCase):
     def test_should_not_show_navbar_if_not_logged_in(self):
         survey = self._dummy_survey(password="dummy_password")
 
-        self._post(action="survey", kwargs={"survey_id": survey.pk},
-                   data={"password": survey.password})
-
+        response = self._post(action="survey", kwargs={"survey_id": survey.pk},
+                              data={"password": survey.password})
+        self.assertEquals(response.status_code, 302)
         response = self._get(action="survey", kwargs={"survey_id": survey.pk})
-
         self.assertEquals(response.status_code, 200)
+
         self.assertTrue("hide_navbar" in response.context)
+        self.assertNotContains(response,
+                               u'<div class="navbar navbar-inverse navbar-static-top" role="navigation">')
 
     def test_should_show_navbar_if_logged_in(self):
         self._login()
-        survey = self._dummy_survey(password="dummy_password")
-
-        self._post(action="survey", kwargs={"survey_id": survey.pk},
-                   data={"password": survey.password})
+        survey = self._dummy_survey()
 
         response = self._get(action="survey", kwargs={"survey_id": survey.pk})
-
         self.assertEquals(response.status_code, 200)
+
         self.assertFalse("hide_navbar" in response.context)
+        self.assertContains(response,
+                            u'<div class="navbar navbar-inverse navbar-static-top" role="navigation">')
