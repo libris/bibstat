dac95d2731a439d063188e3cd62c917a333dc8d1
Viktor Lövgren <viktor.lovgren@valtech.se>
2014-11-07 13:53:02 +0100

    Remove ie-fixes for now since log is not needed; move document ready javascript code out of callback.

--- static/js/app/main.js
+++ static/js/app/main.js
@@ -13,7 +13,7 @@ define(['jquery', 'survey', 'login', 'dispatches', 'libraries', 'jquery.tablesor
         /* Make Variables table sortable */
         $(".table.variables").addClass("tablesorter").tablesorter({
             sortList: [
-                [ 0, 0 ]
+                [0, 0]
             ]
         });
 
@@ -29,61 +29,61 @@ define(['jquery', 'survey', 'login', 'dispatches', 'libraries', 'jquery.tablesor
                 }
             },
             sortList: [
-                [ 3, 0 ]
+                [3, 0]
             ]
         });
-
-        $(".table .select-all").change(function () {
-            var checkboxes = $(".select-one");
-            if ($(this).is(":checked")) {
-                checkboxes.prop("checked", "checked");
-            } else {
-                checkboxes.removeAttr("checked");
-            }
-        });
-
-        $(".select-one, .select-all").change(function () {
-            var checked = $(".select-one:checked").length > 0;
-            var buttons = $(".btn-toggle");
-
-            if (checked) buttons.removeClass("disabled");
-            else buttons.addClass("disabled");
-        });
-
-        /* Edit variable */
-        $(".edit-variable").click(function (ev) {
-            ev.preventDefault(); // prevent navigation
-            var url = $(this).data("form"); // get form
-            $("#variableModal").load(url, function () { // load the url into the modal
-                $(this).modal('show'); // display the modal on url load
-            });
-            return false; // prevent the click propagation
-        });
-
-        /* Create variable */
-        $(".create-variable").click(function (ev) {
-            ev.preventDefault(); // prevent navigation
-            var url = $(this).data("form"); // get form
-            $("#variableModal").load(url, function () { // load the url into the modal
-                $(this).modal('show'); // display the modal on url load
-            });
-            return false; // prevent the click propagation
-        });
-
-        $('.show-dispatch').click(function(e) {
-            e.preventDefault();
-
-            var element = $(this);
-            $('.modal-message .modal-title').html(element.data('title'));
-            $('.modal-message .modal-body').html(element.data('message'));
-            $('.modal-message').modal('show');
-        });
-
-        survey.init();
-        login.init();
-        dispatches.init();
-        libraries.init();
-
-        console.log('Scripts initialized.')
     });
+
+    $(".table .select-all").change(function () {
+        var checkboxes = $(".select-one");
+        if ($(this).is(":checked")) {
+            checkboxes.prop("checked", "checked");
+        } else {
+            checkboxes.removeAttr("checked");
+        }
+    });
+
+    $(".select-one, .select-all").change(function () {
+        var checked = $(".select-one:checked").length > 0;
+        var buttons = $(".btn-toggle");
+
+        if (checked) buttons.removeClass("disabled");
+        else buttons.addClass("disabled");
+    });
+
+    /* Edit variable */
+    $(".edit-variable").click(function (ev) {
+        ev.preventDefault(); // prevent navigation
+        var url = $(this).data("form"); // get form
+        $("#variableModal").load(url, function () { // load the url into the modal
+            $(this).modal('show'); // display the modal on url load
+        });
+        return false; // prevent the click propagation
+    });
+
+    /* Create variable */
+    $(".create-variable").click(function (ev) {
+        ev.preventDefault(); // prevent navigation
+        var url = $(this).data("form"); // get form
+        $("#variableModal").load(url, function () { // load the url into the modal
+            $(this).modal('show'); // display the modal on url load
+        });
+        return false; // prevent the click propagation
+    });
+
+    $('.show-dispatch').click(function (e) {
+        e.preventDefault();
+
+        var element = $(this);
+        $('.modal-message .modal-title').html(element.data('title'));
+        $('.modal-message .modal-body').html(element.data('message'));
+        $('.modal-message').modal('show');
+    });
+
+    survey.init();
+    login.init();
+    dispatches.init();
+    libraries.init();
+
+    console.log('Scripts initialized.')
 });
