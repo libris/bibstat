a820abb15912569a393226ca572a245d832c2398
Viktor Lövgren <viktor.lovgren@valtech.se>
2014-11-18 14:58:13 +0100

    Fix typo in survey template causing errornous summations.

--- static/js/app/survey/cell.js
+++ static/js/app/survey/cell.js
@@ -3,13 +3,11 @@ define(['jquery'], function($) {
         disabled: function(element) { return $(element).prop('disabled'); },
         disable: function(element) {
             $(element).next(".input-group-btn").children(".btn-dropdown").prop("disabled", true);
-            $('#survey-form').bootstrapValidator('enableFieldValidators', $(element).attr('name'), false);
             $(element).prop('disabled', true);
         },
         enabled: function(element) { return !cell.disabled(element); },
         enable: function(element) {
             $(element).next(".input-group-btn").children(".btn-dropdown").prop("disabled", false);
-            $('#survey-form').bootstrapValidator('enableFieldValidators', $(element).attr('name'), true);
             $(element).prop('disabled', false);
         },
         onChange: function(element, callback) { $(element).on("change paste keyup", callback); },
