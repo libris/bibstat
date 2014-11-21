acb82ef2c00af6df2de12f9202d8600e19ccd2bf
Niclas Nilsson <niclas.love.nilsson@gmail.com>
2014-11-21 14:43:04 +0100

    Don't allow creating new rounds for already existing years

--- libstat/views/administration.py
+++ libstat/views/administration.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 
+from libstat.models import Survey
 from libstat.views.surveys import _create_new_collection
 from libstat.survey_templates import available_years
 
@@ -12,6 +13,7 @@ from libstat.survey_templates import available_years
 def administration(request):
     context = {
         "possible_year_choices": available_years(),
+        "message": request.session.pop("message", None),
         "nav_administration_css": "active"
     }
 
@@ -21,6 +23,10 @@ def administration(request):
 @permission_required('is_superuser', login_url='index')
 def create_new_collection(request):
     year = request.POST.get("year")
-    if year:
+
+    if int(year) in [int(year) for year in Survey.objects.distinct("sample_year")]:
+        request.session["message"] = "Kan inte skapa en ny omgång för {}, den omgången finns redan".format(year)
+        return redirect(reverse("administration"))
+    elif year:
         _create_new_collection(year)
     return redirect(reverse('surveys'))
