7b11fac524ee0ca3c88ce183af6f92be38fa571e
Viktor Lövgren <viktor.lovgren@valtech.se>
2014-11-26 08:41:02 +0100

    Use empty value in dispatches if library name or city is missing.

--- libstat/views/dispatches.py
+++ libstat/views/dispatches.py
@@ -13,8 +13,8 @@ def _rendered_template(template, survey):
     survey_url = settings.API_BASE_URL + reverse('survey', args=(survey.id,))
     survey_url_with_password = survey_url + "?p=" + survey.password
 
-    rendered = template.replace(u"{bibliotek}", survey.library.name)
-    rendered = rendered.replace(u"{ort}", survey.library.city)
+    rendered = template.replace(u"{bibliotek}", survey.library.name if survey.library.name else u"")
+    rendered = rendered.replace(u"{ort}", survey.library.city if survey.library.city else u"")
     rendered = rendered.replace(u"{lösenord}", survey.password)
     rendered = rendered.replace(u"{enkätadress}", survey_url)
     rendered = rendered.replace(u"{enkätadress (med lösenord)}", survey_url_with_password)
