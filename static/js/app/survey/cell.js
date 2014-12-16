a03f5bcddaa07a9547c316c06c610e2899658310
Viktor Lövgren <viktor.lovgren@valtech.se>
2014-12-16 10:47:13 +0100

    Fix field validation not being reset when disabling fields as part of summation.

--- static/js/app/survey/cell.js
+++ static/js/app/survey/cell.js
@@ -2,13 +2,22 @@ define(['jquery'], function($) {
     var cell = {
         disabled: function(element) { return $(element).prop('disabled'); },
         disable: function(element) {
-            $(element).next(".input-group-btn").children(".btn-dropdown").prop("disabled", true);
-            $(element).prop('disabled', true);
+            var element = $(element);
+
+            if(element.length > 0) {
+                $('#survey-form').data('bootstrapValidator').resetField(element);
+                element.next(".input-group-btn").children(".btn-dropdown").prop("disabled", true);
+                element.prop('disabled', true);
+            }
         },
         enabled: function(element) { return !cell.disabled(element); },
         enable: function(element) {
-            $(element).next(".input-group-btn").children(".btn-dropdown").prop("disabled", false);
-            $(element).prop('disabled', false);
+            var element = $(element);
+
+            if(element.length > 0) {
+                element.next(".input-group-btn").children(".btn-dropdown").prop("disabled", false);
+                element.prop('disabled', false);
+            }
         },
         onChange: function(element, callback) { $(element).on("change paste keyup", callback); },
         value: function(element) { return $.trim($(element).val()); },
