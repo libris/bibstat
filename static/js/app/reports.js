3dea2846a7547c6e38c81b369a0981d5b416f9ad
Niclas Nilsson <niclas.love.nilsson@gmail.com>
2015-01-13 11:21:08 +0100

    Toggle checkboxes with button

--- static/js/app/reports.js
+++ static/js/app/reports.js
@@ -1,28 +1,42 @@
-define(['jquery', 'scroll'], function($, scroll) {
+define(['jquery', 'scroll'], function ($, scroll) {
     return {
-       'init': function() {
-           $('.btn-checked').click(function(e) {
-               e.preventDefault();
-               $('.checkbox-survey').prop("checked", "checked");
-           });
+        'init': function () {
+            $('.checkbox-survey').change(function () {
+                if ($('.checkbox-survey:not(:checked)').length > 0) {
+                    $('.btn-checked').text("Markera alla");
+                } else {
+                    $('.btn-checked').text("Avmarkera alla");
+                }
+            });
 
-           $('#form-libraries-submit').click(function(e) {
-               var anyChecked = $('.checkbox-survey:checked').length > 0;
-               if(anyChecked) {
-                   $('#form-libraries').append('<input type="hidden" name="previous_url" value="' + window.location.href + '">')
-               } else {
-                   e.preventDefault();
-                   $('.row-alert').removeClass("hidden")
-               }
-           });
+            $('.btn-checked').click(function (e) {
+                e.preventDefault();
 
-           $('.report-explanation').click(function(e) {
-               e.preventDefault();
-           });
+                if($('.checkbox-survey:not(:checked)').length > 0) {
+                    $('.checkbox-survey').prop("checked", "checked");
+                } else {
+                    $('.checkbox-survey').prop("checked", false);
+                }
+                $('.checkbox-survey').first().trigger("change");
+            });
 
-           var scrollStart = $('.scroll-start');
-           if(scrollStart.length > 0)
-               scroll.to(scrollStart);
-       }
-   }
+            $('#form-libraries-submit').click(function (e) {
+                var anyChecked = $('.checkbox-survey:checked').length > 0;
+                if (anyChecked) {
+                    $('#form-libraries').append('<input type="hidden" name="previous_url" value="' + window.location.href + '">')
+                } else {
+                    e.preventDefault();
+                    $('.row-alert').removeClass("hidden")
+                }
+            });
+
+            $('.report-explanation').click(function (e) {
+                e.preventDefault();
+            });
+
+            var scrollStart = $('.scroll-start');
+            if (scrollStart.length > 0)
+                scroll.to(scrollStart);
+        }
+    }
 });
