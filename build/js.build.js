2611ae131ef030cdfd56016aec25ec7bc4e7a5b9
Ricard Fredin <ricard.fredin@valtech.se>
2015-03-25 16:11:33 +0100

    Added new requirejs javascript file for survey page stripped of unnecessary plugins and modules. Added build step for survey-only javascript. Added new html page wrapper for survey page.

--- build/js.build.js
+++ build/js.build.js
@@ -4,4 +4,11 @@
 	mainConfigFile: '../static/js/main.js',
 	include: ['plugins/requirejs/require.js'],
 	out: "../static/js/main.min.js"
+},
+{
+    baseUrl: '../static/js/',
+    name: 'main_survey',
+    mainConfigFile: '../static/js/main_survey.js',
+    include: ['plugins/requirejs/require.js'],
+    out: "../static/js/main_survey.min.js"
 })
