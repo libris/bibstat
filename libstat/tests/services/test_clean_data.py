100ca08379a82109751be971d6b551a36d5fc3ad
katste <ina.stempel@gmail.com>
2015-03-31 17:33:01 +0200

    Added sigel matching against xlsx-mapping file

--- libstat/tests/services/test_clean_data.py
+++ libstat/tests/services/test_clean_data.py
@@ -9,8 +9,13 @@ class TestCleanDataFunctions(MongoTestCase):
         library = self._dummy_library(sigel=random_sigel)
         survey1 = self._dummy_survey(library=library, observations=[self._dummy_observation()])
         survey2 = self._dummy_survey()
-        clean_data._update_sigel(survey1, survey2)
-        self.assertEqual(survey1.library.sigel, survey2.library.sigel)
-        self.assertTrue(survey1.library.sigel in survey1.selected_libraries)
+        clean_data._update_sigel(survey1, survey2.library.sigel)
+        self.assertEqual(survey1.reload().library.sigel, survey2.library.sigel)
+        self.assertTrue(survey1.reload().library.sigel in survey1.selected_libraries)
         self.assertTrue(random_sigel not in survey1.selected_libraries)
-        self.assertEqual(survey1._status, u"published")
+        self.assertEqual(survey1.reload()._status, u"published")
+
+
+    def test_load_sigel_mapping_from_workbook(self):
+        sigel_dict = clean_data._load_sigel_mapping_from_workbook()
+        self.assertEqual(sigel_dict["8aad"], "F")
