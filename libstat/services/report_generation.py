7e28deeb30821f6e63ed6e02c4a4118f4a16a4e5
katste <ina.stempel@gmail.com>
2015-04-27 13:27:39 +0200

    Typos in report text, reverting report template without target group calculations

--- libstat/services/report_generation.py
+++ libstat/services/report_generation.py
@@ -36,14 +36,8 @@ def get_report(surveys, year):
         return cached_report
     else:
 
-        library_types = [survey.library.library_type for survey in surveys]
-
         # This should of course be updated when (and if) more templates are added
-        # Use template without target group calculations if surveys with different library types are included in report
-        if len(surveys) > 1 and not all(libtype == library_types[0] for libtype in library_types):
-            report_template = report_template_2014_without_target_group_calculations()
-        else:
-            report_template = report_template_2014()
+        report_template = report_template_2014()
 
         observations = pre_cache_observations(report_template, surveys, year)
 
