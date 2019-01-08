cea5ccebbc4cbcf6ce05405a1517b2e4762ec64d
Jani Bonnevier <jani.bonnevier@kb.se>
2019-01-08 10:46:25 +0100

    Revert "Specify new intro text variable key for 2018"
    This reverts commit 696f34196d66bfeeefca8ffd432ca7c233d8e8ed.

--- libstat/survey_templates.py
+++ libstat/survey_templates.py
@@ -2,9 +2,9 @@
 from libstat.models import Section, Group, Cell, Row, SurveyTemplate
 
 
-def _survey_template_base(intro_text_variable_key="Introtext2014"):
+def _survey_template_base():
     return SurveyTemplate(
-        intro_text_variable_key=intro_text_variable_key,
+        intro_text_variable_key="Introtext2014",
         sections=[
             Section(title=u"Frågor om biblioteksorganisationen",
                     groups=[
@@ -505,8 +505,7 @@ def _default_template_from_survey(survey):
 
 def survey_template(year, survey=None):
     year = int(year)
-    if year >= 2018:
-        return _survey_template_base(intro_text_variable_key="Introtext2018")
     if year >= 2014:
-        return _survey_template_base(intro_text_variable_key="Introtext2014")
+        return _survey_template_base()
+
     return _default_template_from_survey(survey)
