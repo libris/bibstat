6c54e0371a69d6989be37107c5392439c1e520c6
Jesper Engström <jesper.engstrom@kb.se>
2020-03-13 17:59:04 +0100

    Revert to popover trigger: click

--- static/js/app/survey.js
+++ static/js/app/survey.js
@@ -279,7 +279,7 @@ define(['jquery', 'bootbox', 'survey.sum', 'survey.cell', 'surveys.dispatch', 'b
             $('.btn-help').popover({
                 container: 'body',
                 html: true,
-                trigger: 'focus'
+                trigger: 'click'
             }).click(function (e) {
                 e.preventDefault();
             });
